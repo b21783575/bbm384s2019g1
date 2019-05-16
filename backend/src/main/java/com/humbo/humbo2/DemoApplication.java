9dc07ca924779383cf684efa3e342ddfacd8d26d
Hasan Akalp <hasan.akalp@gmail.com>
2019-05-16 22:18:44 +0300

    ADD CORS

--- backend/src/main/java/com/humbo/humbo2/DemoApplication.java
+++ backend/src/main/java/com/humbo/humbo2/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.humbo.humbo2.storage.StorageProperties;
 import com.humbo.humbo2.storage.StorageService;
@@ -24,6 +26,18 @@ public class DemoApplication {
         return (args) -> {
             storageService.deleteAll();
             storageService.init();
+        };
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**/*").allowedOrigins("http://localhost:8888");
+                registry.addMapping("/**").allowedOrigins("http://localhost:8888");
+                registry.addMapping("/").allowedOrigins("http://localhost:8888");
+            }
         };
     }
 }
