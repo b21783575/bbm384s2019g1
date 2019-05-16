431402e212f4c737318c9720f99df2e18f40b76e
Hasan Akalp <hasan.akalp@gmail.com>
2019-05-16 17:30:41 +0300

    SET CrossOrigin for basicauth

--- backend/src/main/java/com/humbo/humbo2/controller/BasicAuthController.java
+++ backend/src/main/java/com/humbo/humbo2/controller/BasicAuthController.java
@@ -1,9 +1,11 @@
 package com.humbo.humbo2.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins={ "http://localhost:8080", "http://localhost:8888" })
 public class BasicAuthController {
 
     @GetMapping(path = "/basicauth")
